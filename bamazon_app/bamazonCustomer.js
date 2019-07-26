5b5959feb19389f99ba0c1012406af84f3ab3017
rod080 <49286063+rod080@users.noreply.github.com>
2019-07-26 15:25:31 -0700

    lets user know how much is left in inventory

--- bamazon_app/bamazonCustomer.js
+++ bamazon_app/bamazonCustomer.js
@@ -7,18 +7,12 @@ var mysql = require("mysql");
 var inquirer = require("inquirer");
 var connection = mysql.createConnection({
   host: "localhost",
-
-  // Your port; if not 3306
   port: 3306,
-
-  // Your username
   user: "root",
 
-  // Your password
   password: "@Dolphin080",
   database: "bamazon"
 });
-
 connection.connect(function (err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId);
@@ -32,7 +26,6 @@ function startPage() {
     userInquirer();
   });
 }
-
 function userInquirer() {
   // userInquirer()//this funtion probalbly deosnt/shouldnt go here
   inquirer
@@ -65,15 +58,17 @@ function userInquirer() {
                 }
                 else if (user.quantity > res[0].stock || res[0].stock < 1) {
                   console.log("can't make purchase, not enough in inventory")
+                  userInquirer();
                 }
                 else {
                   console.log("You have chosen the item with id " + id);
                   // console.log(res);
                   var initialStock = parseInt(res[0].stock);
                   var finalStock = initialStock - quantity;
-                  console.log(finalStock);
-                  //   connection.end(); hmmm what does this line do?
                   updater(id, finalStock);
+                  console.log('Congrats on yout purchase! there are ' + finalStock + ' left');
+                  //   connection.end(); hmmm what does this line do?
+                 
                 }
               });
             }
@@ -84,14 +79,18 @@ function userInquirer() {
 }
 function updater(id, finalStock) {
   var sql = "UPDATE products SET stock = " + finalStock + " WHERE id = " + id;
-
   connection.query(sql, function (err, result) {
-    if (err) throw err;
-    console.log(result.affectedRows + " purchase succesful!");
-    userInquirer();
+    if (err) {
+      throw err
+    }
+    else{
+      console.log("purchase succesful!");
+      userInquirer();
+    }
+    startPage();
+
+    
   });
 };
-
-
 
 
